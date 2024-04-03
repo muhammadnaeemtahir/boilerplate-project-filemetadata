52d7bde5f19f465b8d6fe798e6054fe16ff5167f
muhammadnaeemtahir <m.naeemtahir@hotmail.com>
2024-04-03 17:37:25 +0500

    add: '/api/fileanalyse' API endpoint functionaity

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 require('dotenv').config()
 
 var app = express();
@@ -12,9 +14,22 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
 
+  console.log('File uploaded:', req.file.originalname);
+
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log(`http://localhost:${port}/`)
 });
